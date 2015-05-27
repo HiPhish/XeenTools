990c47ad43fb8c17b1c2f34153b367345d220d22
HiPhish <hiphish@Aleksandars-iMac.local>
2015-05-27 10:14:37 +0200

    Removed ATT, MON and FAC tool functions.
    Those functions were really just a wrapper to extract sprites with a
    limit for the index. That's really redundant when we have the limit as
    a constant anyway.

--- Source/att/att_tool.h
+++ Source/att/att_tool.h
@@ -20,26 +20,5 @@ enum xeen_att_frame {
 	XEEN_ATT_FRAMES , /**< Number of ATT frames. */
 };
 
-/** Get a frame from an ATT file.
- *
- *  @param f      Index of the frame.
- *  @param s      Sprite of the ATT file
- *  @param frame  Where to store the resulting frame.
- *  @param t      Transparency index.
- *
- *  @return  Error code:
- *             - 0: Success
- *             - 1: Invalid arguments
- *             - Otherwise error code of `xeen_get_frame` +1
- *
- *  @pre The sprite must have a valid frame map and cells array.
- *  @pre The frame pointer must be valid.
- *  @pre The frame pointer's pixel array must be invalid.
- *  @pre The index must be within the range of ATT frames.
- *
- *  @post On success the frame's pixel array will be allocated and valid.
- */
-int xeen_att_get_frame(enum xeen_att_frame f, XeenSprite s, XeenFrame *frame, uint8_t t);
-
 #endif /* XEEN_ATT_TOOL_H */
 
--- Source/mon/mon_tool.h
+++ Source/mon/mon_tool.h
@@ -23,26 +23,5 @@ enum xeen_mon_frame {
 	XEEN_MON_FRAMES , /**< Number of MON frames. */
 };
 
-/** Get a frame from a MON file.
- *
- *  @param f      Index of the frame.
- *  @param s      Sprite of the MON file
- *  @param frame  Where to store the resulting frame.
- *  @param t      Transparency index.
- *
- *  @return  Error code:
- *             - 0: Success
- *             - 1: Invalid arguments
- *             - Otherwise error code of `xeen_get_frame` +1
- *
- *  @pre The sprite must have a valid frame map and cells array.
- *  @pre The frame pointer must be valid.
- *  @pre The frame pointer's pixel array must be invalid.
- *  @pre The index must be within the range of MON frames.
- *
- *  @post On success the frame's pixel array will be allocated and valid.
- */
-int xeen_mon_get_frame(enum xeen_mon_frame f, XeenSprite s, XeenFrame *frame, uint8_t t);
-
 #endif /* XEEN_MON_TOOL_H */
 
