95a237a17b5e5f20c0ff81bd052461f4485e8237
HiPhish <hiphish@Aleksandars-iMac.local>
2015-05-31 22:18:29 +0200

    Reading map files

--- Source/dat/dat_tool.c
+++ Source/dat/dat_tool.c
@@ -1,4 +1,15 @@
+#include <assert.h>
+#include <stdlib.h>
 #include "dat_tool.h"
+
+#define XEEN_MAP_WIDTH   16
+#define XEEN_MAP_HEIGHT  16
+
+
+int xeen_map_size[XEEN_COORDS] = {
+	[XEEN_X] = XEEN_MAP_WIDTH,
+	[XEEN_Y] = XEEN_MAP_HEIGHT,
+};
 
 int xeen_read_sprite_info(FILE *fp, long o, int i, XeenSpriteFrameInfo *info) {
 	enum {
@@ -45,6 +56,141 @@ int xeen_read_sprite_info(FILE *fp, long o, int i, XeenSpriteFrameInfo *info) {
 			[XEEN_SPRITE_RIGHT] = bytes[2 * XEEN_SPRITE_DIRECTIONS + XEEN_SPRITE_RIGHT],
 		},
 	};
+
+end:
+	return error;
+}
+
+int xeen_read_outdoor_map(FILE *fp, long o, XeenOutdoorMap *map) {
+	enum {
+		SUCCESS,
+		INVALID_ARGS,
+		FREAD_FAIL,
+		MALLOC_FAIL,
+	} error = SUCCESS;
+
+	/* Pre-conditions */
+	if (!fp || !map) {
+		return INVALID_ARGS;
+		goto end;
+	} else if (map->tile) {
+		error = INVALID_ARGS;
+		goto end;
+	}
+
+	XeenOutdoorTile *tile = NULL;
+
+	/* Jump to position */
+	if (fseek(fp, o, SEEK_SET) != 0) {
+		error = FREAD_FAIL;
+		goto end;
+	}
+
+	#define TILES  (XEEN_MAP_WIDTH * XEEN_MAP_HEIGHT)
+	uint8_t bytes[3 * TILES + 64];
+
+	if (fread(bytes, sizeof(uint8_t), 3 * TILES, fp) != 3 * TILES * sizeof(uint8_t)) {
+		error = FREAD_FAIL;
+		goto end;
+	}
+
+	if (!(tile = malloc(TILES * sizeof(*tile)))) {
+		error = MALLOC_FAIL;
+		goto end;
+	}
+
+	for (int y = 0; y < XEEN_MAP_HEIGHT; ++y) {
+		for (int x = 0; x < XEEN_MAP_WIDTH; ++x) {
+			int i = y * XEEN_MAP_HEIGHT + x; 
+			int j = 2*y * XEEN_MAP_HEIGHT + 2*x;
+
+			/* Wall data */
+			tile[i].ground  = bytes[j + 0] & 0x0F;
+			tile[i].middle  = bytes[j + 0] & 0xF0;
+			tile[i].top     = bytes[j + 1] & 0x0F;
+			tile[i].overlay = bytes[j + 1] & 0xF0;
+			assert((tile[i]).ground < 16);
+
+			/* Flags */
+			tile[i].flags = bytes[2 * TILES + y * XEEN_MAP_HEIGHT + x];
+		}
+	}
+
+	*map = (XeenOutdoorMap) {
+		.tile = tile,
+
+		/* Skip two bytes because they possibly incorrect and not needed */
+
+		#define OFF(n)  (3 * TILES + n)
+		.map_n = bytes[OFF(2)],
+		.map_e = bytes[OFF(3)],
+		.map_s = bytes[OFF(4)],
+		.map_w = bytes[OFF(5)],
+
+		.maze_flag_1 = ((uint16_t)bytes[OFF(7)] << 8) + (uint16_t)bytes[OFF(6)],
+		.maze_flag_2 = ((uint16_t)bytes[OFF(9)] << 8) + (uint16_t)bytes[OFF(8)],
+
+		.wall_types = {
+			[ 0] = bytes[OFF(10) +  0],
+			[ 1] = bytes[OFF(10) +  1],
+			[ 2] = bytes[OFF(10) +  2],
+			[ 3] = bytes[OFF(10) +  3],
+			[ 4] = bytes[OFF(10) +  4],
+			[ 5] = bytes[OFF(10) +  5],
+			[ 6] = bytes[OFF(10) +  6],
+			[ 7] = bytes[OFF(10) +  7],
+			[ 8] = bytes[OFF(10) +  8],
+			[ 9] = bytes[OFF(10) +  9],
+			[10] = bytes[OFF(10) + 10],
+			[11] = bytes[OFF(10) + 11],
+			[12] = bytes[OFF(10) + 12],
+			[13] = bytes[OFF(10) + 13],
+			[14] = bytes[OFF(10) + 14],
+			[15] = bytes[OFF(10) + 15],
+		},
+
+		.floor_types = {
+			[ 0] = bytes[OFF(26) +  0],
+			[ 1] = bytes[OFF(26) +  1],
+			[ 2] = bytes[OFF(26) +  2],
+			[ 3] = bytes[OFF(26) +  3],
+			[ 4] = bytes[OFF(26) +  4],
+			[ 5] = bytes[OFF(26) +  5],
+			[ 6] = bytes[OFF(26) +  6],
+			[ 7] = bytes[OFF(26) +  7],
+			[ 8] = bytes[OFF(26) +  8],
+			[ 9] = bytes[OFF(26) +  9],
+			[10] = bytes[OFF(26) + 10],
+			[11] = bytes[OFF(26) + 11],
+			[12] = bytes[OFF(26) + 12],
+			[13] = bytes[OFF(26) + 13],
+			[14] = bytes[OFF(26) + 14],
+			[15] = bytes[OFF(26) + 15],
+		},
+
+		.floor_type   = bytes[OFF(42)],
+		.run_x        = bytes[OFF(43)],
+		.wall_no_pass = bytes[OFF(44)],
+		.surf_no_pass = bytes[OFF(45)],
+		.unlock_door  = bytes[OFF(46)],
+		.unlock_box   = bytes[OFF(47)],
+		.bash_door    = bytes[OFF(48)],
+		.bash_grate   = bytes[OFF(49)],
+		.bash_wall    = bytes[OFF(50)],
+		.run_chance   = bytes[OFF(51)],
+		.run_y        = bytes[OFF(52)],
+		.trap_dmg     = bytes[OFF(53)],
+		.wall_type    = bytes[OFF(54)],
+		.tavern_tips  = bytes[OFF(55)],
+
+		.seen = ((uint32_t)bytes[OFF(59)] << 24) +((uint32_t)bytes[OFF(58)] << 16)
+		      + ((uint32_t)bytes[OFF(57)] <<  8) +((uint32_t)bytes[OFF(56)] <<  0),
+
+		.stepped = ((uint32_t)bytes[OFF(63)] << 24) +((uint32_t)bytes[OFF(62)] << 16)
+		         + ((uint32_t)bytes[OFF(61)] <<  8) +((uint32_t)bytes[OFF(60)] <<  0),
+		#undef OFF
+	};
+	#undef TILES
 
 end:
 	return error;
--- Source/dat/dat_tool.h
+++ Source/dat/dat_tool.h
@@ -5,18 +5,22 @@
  *
  *  Interface for DAT files.
  *
- *  A DAT file contains sprite information on what sprite of an object to
- *  display for direction. There are four directions, all relative to given
- *  relative to the player.
+ *  There are two kind of DAT files: One is called "CLOUDS.DAT" or "DARK.DAT"
+ *  and contains sprite information on what sprite of an object to display for
+ *  which direction. The other type as the "MAZExxxx.DAT" files that store map
+ *  layout.
  *
- *  Every information consists of the index of the first frame, whether the
- *  sprite is flipped horizontally and the index of the last frame. Flipping
- *  allows the reuse of frames, for example when viewing an object from the
- *  left of the right.
+ *  For sprites there are four directions, all relative to given relative to
+ *  the player.  Every sprite information consists of the index of the first
+ *  frame, whether the sprite is flipped horizontally and the index of the last
+ *  frame.  Flipping allows the reuse of frames, for example when viewing an
+ *  object from the left of the right.
  */
 
 #include <stdint.h>
 #include <stdio.h>
+
+#include "../xeen/xeen.h"
 
 /** Number of objects described in a DAT file. */
 #define XEEN_DAT_OBJECTS  121
@@ -37,6 +41,78 @@ typedef struct xeen_sprite_frame_info {
 	uint8_t last  [XEEN_SPRITE_DIRECTIONS]; /**< Last frame.                   */
 } XeenSpriteFrameInfo;
 
+enum xeen_outdoor_flag {
+	XEEN_OUTDOOR_F_MON_L  = 0X01,
+	XEEN_OUTDOOR_F_MON_C  = 0X02,
+	XEEN_OUTDOOR_F_MON_R  = 0X04,
+	XEEN_OUTDOOR_F_OBJECT = 0x08,
+	XEEN_OUTDOOR_F_EVENT  = 0x10,
+	XEEN_OUTDOOR_F_DRAIN  = 0x20,
+	XEEN_OUTDOOR_F_WATER  = 0x40,
+	XEEN_OUTDOOR_F_GRATE  = 0x80,
+	XEEN_OUTDOOR_FLAGS    =    8,
+};
+
+enum xeen_indoor_flag {
+	XEEN_INDOOR_F_INSIDE   = 0x08,
+	XEEN_INDOOR_F_EVENT    = 0x10,
+	XEEN_INDOOR_F_UNKOWN_2 = 0X20,
+	XEEN_INDOOR_F_WATER    = 0x40,
+	XEEN_INDOOR_F_UNKOWN_1 = 0X80,
+	XEEN_INDOOR_FLAGS      =    5,
+};
+
+typedef struct xeen_outdoor_tile {
+	uint8_t ground;
+	uint8_t middle;
+	uint8_t top;
+	uint8_t overlay;
+	uint8_t flags;
+} XeenOutdoorTile;
+
+typedef struct xeen_outdoor_map {
+	XeenOutdoorTile *tile;
+
+	uint16_t map_n;
+	uint16_t map_e;
+	uint16_t map_s;
+	uint16_t map_w;
+
+	uint16_t maze_flag_1;
+	uint16_t maze_flag_2;
+
+	uint8_t wall_types  [16];
+	uint8_t floor_types [16];
+
+	uint8_t wall_type;
+	uint8_t floor_type;
+	uint8_t run_chance;
+	uint8_t run_x;
+	uint8_t run_y;
+	uint8_t wall_no_pass;
+	uint8_t surf_no_pass;
+	uint8_t unlock_door;
+	uint8_t unlock_box;
+	uint8_t bash_door;
+	uint8_t bash_grate;
+	uint8_t bash_wall;
+	uint8_t trap_dmg;
+	uint8_t tavern_tips;
+
+	uint32_t seen;
+	uint32_t stepped;
+} XeenOutdoorMap;
+
+typedef struct xeen_indoor_tile {
+	uint8_t wall_n;
+	uint8_t wall_e;
+	uint8_t wall_s;
+	uint8_t wall_w;
+	uint8_t flags;
+} XeenIndoorTile;
+
+extern int xeen_map_size[XEEN_COORDS];
+
 /** Read sprite information form a DAT file.
  *
  *  @param fp    File pointer.
@@ -54,6 +130,8 @@ typedef struct xeen_sprite_frame_info {
  *  @post  The info pointer will contain the result.
  */
 int xeen_read_sprite_info(FILE *fp, long o, int i, XeenSpriteFrameInfo *info);
+
+int xeen_read_outdoor_map(FILE *fp, long o, XeenOutdoorMap *map);
 
 #endif /* XEEN_DAT_TOOL_H */
 
